8043f35ca8d735ceaf268ccd7828613f60ae1969
kalycoding <muhdkhalifa1@yahoo.com>
2021-03-21 23:18:27 +0100

    update

--- blogpost/settings.py
+++ blogpost/settings.py
@@ -130,14 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # new
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # new
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new
-STATICFILES_FINDERS = [
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # new
+# STATICFILES_FINDERS = [
+#     "django.contrib.staticfiles.finders.FileSystemFinder",
+#     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+# ]
 
 
 # Rest Framework Setup
